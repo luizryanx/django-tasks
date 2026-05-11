f2a633df25be301a6260ce4d8f8e3ae617389ea3
luizryanx <luizryanaraujodasilva17@gmail.com>
2026-05-11 01:17:33 -0300

    Atualização: Cadastro de Tarefas

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,3 +5,8 @@ class TasksForm(forms.ModelForm):
     class Meta:
         model = Tasks
         fields = ['title', 'description', 'status']
+
+class TasksFormCreate(forms.ModelForm):
+    class Meta:
+        model = Tasks
+        fields = ['title', 'description', 'status', 'due_date']

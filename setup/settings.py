b7d406874bc6f9d79676c1556defd61c8a354f37
luizryanx <luizryanaraujodasilva17@gmail.com>
2026-05-30 18:17:57 -0300

    adicao de recuperação de senha

--- setup/settings.py
+++ setup/settings.py
@@ -133,3 +133,6 @@ LOGOUT_URL = "logout"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
